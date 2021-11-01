6ef5769e685768b45be609de18f40e5d3667dcae
hlduthie <hana.duthie@gmail.com>
2021-11-01 10:43:06 +1030

    updating notes
    final update before submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//function to set up the current date and time
 
 let days = [
   "Sunday",
@@ -21,20 +22,21 @@ function todaysDay() {
     mins = `0${mins}`;
   }
   return `${day} ${hour}:${mins}`
-
 }
 
 let today = document.querySelector(".currentDay")
 today.innerHTML = todaysDay();
 
+// function to get the lat and long for the forecast
+
 function getForecast(coordinates) {
   let apiKey = "1fb819a70022e5b5b0ca00fd56103479";
-
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`  
-  console.log(coordinates);
-  console.log(apiURL);
   axios.get(apiURL).then(displayForecast);
 }
+
+// to display the name of the city, current temp, wind speed, weather description, display current temp icon
+// and call the getForecast function
 
 function showTemperature(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -42,15 +44,14 @@ function showTemperature(response) {
     celciusTemperature = response.data.main.temp);
   document.querySelector("#wind2").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#description2").innerHTML = response.data.weather[0].description;
-  console.log(response.data);
 
   let currentIcon = document.querySelector("#currentTemp-icon");
   currentIcon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
-  
-
   getForecast(response.data.coord);
 }
+
+// formatting the api timestamp from dt numbers to the name of a day
 
 function formatDt(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -59,13 +60,12 @@ function formatDt(timestamp) {
   return days[day];
 }
 
+// setting up the forecast for the next 6 days 
+// index is the the numbers for the dt (0-8) - the next 8 days on the api
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
-
   let forecastHTML = `<div class="row">`;
-  
   forecast.forEach(function(forecastDay, index) {
     if (index > 0 && index < 7) {
     forecastHTML = forecastHTML +
@@ -83,6 +83,8 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+// searching for the city using personel apiKey
+
 function searchCity(city) {
   let apiKey = "1fb819a70022e5b5b0ca00fd56103479";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -90,8 +92,9 @@ function searchCity(city) {
 
   axios.get(apiUrl).then(showTemperature);
   axios.get(apiUrl_followingForecast).then(displayForecast);
-
 }
+
+// action after the user enters a city name
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -103,9 +106,6 @@ let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", handleSubmit);
 
 searchCity("New York");
-
-
-
 
 function searchLocation(position) {
   let apiKey = "1fb819a70022e5b5b0ca00fd56103479";
